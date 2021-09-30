79a2ef1f1eb63231153364fb945792e8f53b4f4e
mmotya <kevdina@yandex.ru>
2021-09-30 20:00:53 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -26,3 +26,9 @@ var hotelSlider = new Swiper('.hotel-slider', {
       pageUpDown: true,
     },
   });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function() {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  })
