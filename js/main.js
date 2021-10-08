1b084f626328bcfd5da26a971dfeefbb2f293cb8
mmotya <kevdina@yandex.ru>
2021-10-08 21:37:46 +0300

    fix: ошибки из 31 задания

--- js/main.js
+++ js/main.js
@@ -29,11 +29,11 @@ $(document).ready(function () {
   });
 
   var menuButton = document.querySelector(".menu-button");
-  menuButton.addEventListener("click", function() {
+  menuButton.addEventListener("click", function () {
     console.log("Клик по кнопке меню");
     document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
   });
-  
+
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
@@ -52,8 +52,8 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
-  $(document).on('keydown', function(e){
-    if(e.which === 27){
+  $(document).on('keydown', function (e) {
+    if (e.which === 27) {
       var modalOverlay = $(".modal__overlay");
       var modalDialog = $(".modal__dialog");
       modalOverlay.removeClass("modal__overlay--visible");
@@ -66,36 +66,65 @@ $(document).ready(function () {
     $(this).validate({
       errorClass: "invalid",
       messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Please enter your real name",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        subscribe: {
+          required: "Need email address",
+          email: "Format of name@domain.com",
+        },
+        phone: {
+          required: "Phone number required",
+          minlength: "Incorrect number"
+        },
+      },
+    });
+  });
+
+  //обработка форм
+  $(".newsletter__subscribe").each(function () {
+    $(this).validate({
+      errorClass: "invalide",
+      messages: {
         name: "Please specify your name",
         email: {
           required: "We need your email address to contact you",
           email: "Your email address must be in the format of name@domain.com",
         },
-        phone:{
+        subscribe: {
+          required: "Need email address",
+          email: "Format of name@domain.com",
+        },
+        phone: {
           required: "Phone number required",
         },
       },
     });
   });
-  
-  $(".form").ready(function(){
-     $('.footer__input--phone').mask('+7 (ZZZ) ZZZ-ZZ-ZZ' , {
-        translation: {
-          'Z': {
+
+  $(".form").ready(function () {
+    $('.footer__input--phone').mask('+7 (ZZZ) ZZZ-ZZ-ZZ', {
+      translation: {
+        'Z': {
           pattern: /[0-9]/, optional: true
         }
       }
     });
   });
-  $(".form").ready(function(){
-     $('.modal__input--phone').mask('+7 (ZZZ) ZZZ-ZZ-ZZ' , {
-        translation: {
-          'Z': {
+  $(".form").ready(function () {
+    $('.modal__input--phone').mask('+7 (ZZZ) ZZZ-ZZ-ZZ', {
+      translation: {
+        'Z': {
           pattern: /[0-9]/, optional: true
         }
       }
     });
   });
-  
+
   AOS.init();
 });
